0238c2f3a144eaf10ad482522ed3859906fdcbf6
kalanakt <e19198@eng.pdn.ac.lk>
2022-09-03 23:14:00 +0530

    gramscript `v1.0.0`

---
+++ gramscript/__init__.py
@@ -0,0 +1,8 @@
+
+__copyright__ = "Copyright (c) 2022 Gramscript Telegram API"
+__license__ = "MIT License"
+__version__ = "1.0.0"
+
+from .api.errors import Error
+from .client import ChatAction
+from .client import Client
