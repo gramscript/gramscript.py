0a7c948c0440604f4724745b266a7c7250087a26
kalanakt <e19198@eng.pdn.ac.lk>
2022-09-03 23:08:39 +0530

    gramscript `v1.0.0`

---
+++ pyrogram/__init__.py
@@ -0,0 +1,8 @@
+
+__copyright__ = "Copyright (C) 2017-2018 Dan Tès <https://github.com/delivrance>"
+__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
+__version__ = "0.4.0"
+
+from .api.errors import Error
+from .client import ChatAction
+from .client import Client
